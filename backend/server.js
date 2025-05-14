f2b71bcfc1570b69ca8889f49d49892bb4c73617
majoralpaca984 <eleonora.troiani84@gmail.com>
2025-05-14 18:11:01 +0200

    modifica di cors

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware globali
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "https://mystriveblogproject1.vercel.app"],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 // Session & Passport
